b72db513b2dbc0ae1bc104fbba9d39c9762d7a87
npc-z <1763998996@qq.com>
2022-09-25 23:19:21 +0800

    v1.4.1, 打开文件时自动跳转到上次位置

--- lua/auto-commands.lua
+++ lua/auto-commands.lua
@@ -7,3 +7,16 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 		utils.trim_trailing_whitespace()
 	end,
 })
+
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+	pattern = { "*" },
+
+	callback = function()
+		-- 打开文件时自动跳转到上次位置
+		vim.cmd([[
+            if line("'\"") >= 1 && line("'\"") <= line("$")
+            \ |   exe "normal! g`\""
+            \ | endif
+        ]])
+	end,
+})
