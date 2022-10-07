639b19b0607d717c0d999a4c5768d213db1de8b9
npc-z <1763998996@qq.com>
2022-10-07 16:11:35 +0800

    upate dashboard footer

--- lua/plugin-config/dashboard.lua
+++ lua/plugin-config/dashboard.lua
@@ -4,11 +4,18 @@ if not status then
 	return
 end
 
-db.custom_footer = {
-	"",
-	"",
-	-- "fuck this",
-}
+local function make_custom_footer()
+	local datetime = os.date("%Y-%m-%d %H:%M:%S")
+	local default_footer = { "", "Have fun with neovim", datetime }
+
+	if packer_plugins ~= nil then
+		local count = #vim.tbl_keys(packer_plugins)
+		default_footer[3] = "loaded " .. count .. " plugins"
+	end
+	return default_footer
+end
+
+db.custom_footer = make_custom_footer()
 
 db.custom_center = {
 	{
