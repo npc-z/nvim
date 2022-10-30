ced91698890aabc77caf85848777079356d1cdfb
npc-z <1763998996@qq.com>
2022-10-30 22:24:14 +0800

    show symbol in winbar

--- lua/lsp/ui.lua
+++ lua/lsp/ui.lua
@@ -116,10 +116,31 @@ local function __init_lspsaga(saga)
 		-- show symbols in winbar must nightly
 		symbol_in_winbar = {
 			in_custom = false,
-			enable = false,
+			enable = true,
 			separator = " ",
-			show_file = true,
-			click_support = false,
+			show_file = false,
+			click_support = function(node, clicks, button, modifiers)
+				-- To see all avaiable details: vim.pretty_print(node)
+				local st = node.range.start
+				local en = node.range["end"]
+				if button == "l" then
+					if clicks == 2 then
+					-- double left click to do nothing
+					else -- jump to node's starting line+char
+						vim.fn.cursor(st.line + 1, st.character + 1)
+					end
+				elseif button == "r" then
+					if modifiers == "s" then
+						print("lspsaga") -- shift right click to print "lspsaga"
+					end -- jump to node's ending line+char
+					vim.fn.cursor(en.line + 1, en.character + 1)
+				elseif button == "m" then
+					-- middle click to visual select node
+					vim.fn.cursor(st.line + 1, st.character + 1)
+					vim.cmd("normal v")
+					vim.fn.cursor(en.line + 1, en.character + 1)
+				end
+			end,
 		},
 		-- show outline
 		show_outline = {
@@ -131,7 +152,7 @@ local function __init_lspsaga(saga)
 			auto_enter = true,
 			auto_preview = true,
 			virt_text = "┃",
-			jump_key = "o",
+			jump_key = "<CR>",
 			-- auto refresh when change buffer
 			auto_refresh = true,
 		},
