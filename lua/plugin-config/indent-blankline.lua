92a6d451d3141e14f244578010409201dad1d402
npc-z <1763998996@qq.com>
2023-10-07 10:17:38 +0800

    update plugins' version

--- lua/plugin-config/indent-blankline.lua
+++ lua/plugin-config/indent-blankline.lua
@@ -1,48 +1,7 @@
-local status, ident_blankline = pcall(require, "indent_blankline")
+local status, ident_blankline = pcall(require, "ibl")
 if not status then
     vim.notify("没有安装插件: indent_blankline")
     return
 end
 
-ident_blankline.setup({
-    -- 空行占位
-    space_char_blankline = " ",
-    -- 用 treesitter 判断上下文
-    show_current_context = true,
-    show_current_context_start = true,
-    context_patterns = {
-        "class",
-        "function",
-        "method",
-        "element",
-        "^if",
-        "^while",
-        "^for",
-        "^object",
-        "^table",
-        "block",
-        "arguments",
-    },
-    -- 当不需要的界面出现竖线时, 使用下面命令查看文件类型
-    -- 然后将其加入下面的排除列表中
-    -- :echo &filetype
-    filetype_exclude = {
-        "dashboard",
-        "packer",
-        "terminal",
-        "help",
-        "log",
-        "markdown",
-        "TelescopePrompt",
-        "lsp-installer",
-        "lspinfo",
-        "toggleterm",
-    },
-    -- 竖线样式
-    -- char = '¦'
-    -- char = '┆'
-    -- char = '│'
-    -- char = "⎸",
-    char = "▏",
-})
-
+ident_blankline.setup()
