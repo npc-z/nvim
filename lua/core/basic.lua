9a56d73306227acaee0814b26678f60a61fc0117
npc-z <1763998996@qq.com>
2023-10-19 20:08:51 +0800

    fix clipborad config

--- lua/core/basic.lua
+++ lua/core/basic.lua
@@ -4,9 +4,8 @@
 -- vim.bo.{option}: buffer-local 选项
 -- vim.wo.{option}: window-local 选项
 
--- install gvim and do not need set this
--- vim.g.clipboard = "unnamedplus"
-vim.g.clipboard = "unnamed"
+-- install gvim and set this
+vim.o.clipboard = "unnamedplus"
 
 -- utf-8
 vim.g.encoding = "utf-8"
