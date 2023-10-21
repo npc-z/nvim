244efc872d2817f167058ef02bc38a3759b39233
npc-z <1763998996@qq.com>
2023-10-21 10:48:43 +0800

    add rust lsp server and restruct lsp's keymaps

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -9,10 +9,6 @@ return {
         local lspconfig = require("lspconfig")
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-        local keymap = vim.keymap -- for conciseness
-
-        local opts = { noremap = true, silent = true }
-
         local on_attach = function(client, bufnr)
             local illuminate = require("illuminate")
             illuminate.on_attach(client)
@@ -23,56 +19,7 @@ return {
                 client.server_capabilities.documentRangeFormattingProvider = false
             end
 
-            opts.buffer = bufnr
-
-            -- set keybinds
-            opts.desc = "LSP references"
-            keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
-
-            -- opts.desc = "Go to declaration"
-            -- keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-
-            opts.desc = "Show LSP definitions"
-            keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-
-            -- opts.desc = "Show LSP implementations"
-            -- keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
-
-            -- opts.desc = "Show LSP type definitions"
-            -- keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
-
-            -- see available code actions, in visual mode will apply to selection
-            opts.desc = "See available code actions"
-            keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-
-            opts.desc = "Smart rename"
-            keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
-            opts.desc = "Show buffer diagnostics"
-            keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-            opts.desc = "Show line diagnostics"
-            keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-            opts.desc = "Go to previous diagnostic"
-            keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-            opts.desc = "Go to next diagnostic"
-            keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-            opts.desc = "Show documentation for what is under cursor"
-            keymap.set("n", "gh", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-            opts.desc = "Show signature help"
-            keymap.set(
-                "n",
-                "<leader>k",
-                "<cmd>lua vim.lsp.buf.signature_help()<CR>",
-                opts
-            )
-
-            -- opts.desc = "Restart LSP"
-            -- keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+            require("plugins.lsp.after.lspkeymaps").setup_keymaps(bufnr)
         end
 
         vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
@@ -223,6 +170,12 @@ return {
                     },
                 },
             },
+        })
+
+        -- configure rust server
+        lspconfig["rust_analyzer"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
         })
     end,
 }
--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -34,6 +34,7 @@ return {
                 "lua_ls",
                 "pyright",
                 "gopls",
+                "rust_analyzer",
             },
             -- auto-install configured servers (with lspconfig)
             automatic_installation = true, -- not the same as ensure_installed
