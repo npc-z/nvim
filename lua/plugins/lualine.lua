065e413251cb697069d4ce72884adcd4031816b3
npc-z <1763998996@qq.com>
2024-02-06 11:17:03 +0800

    do not show file path in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -31,14 +31,6 @@ return {
             sections = {
                 lualine_c = {
                     {
-                        "filename",
-                        -- 0: Just the filename
-                        -- 1: Relative path
-                        -- 2: Absolute path
-                        -- 3: Absolute path, with tilde as the home directory
-                        path = 1,
-                    },
-                    {
                         "lsp_progress",
                         spinner_symbols = {
                             "🌑 ",
