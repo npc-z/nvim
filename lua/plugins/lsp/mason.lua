576d1aaf4e6bf375bd7df68c1ce52674c480cead
npc-z <1763998996@qq.com>
2024-03-03 17:25:01 +0800

    config mason for nixos

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -13,30 +13,45 @@ return {
 
         local mason_tool_installer = require("mason-tool-installer")
 
-        local ensure_installed = {
+        local ensure_installed_lsp_servers = {
             "tsserver",
             "html",
             "cssls",
             "tailwindcss",
-            "lua_ls",
+            -- "lua_ls",
             "pyright",
             "gopls",
             "rust_analyzer",
             -- "nil_ls",
         }
 
-        -- 当系统为 nixos 时才安装 nil_ls
+        local ensure_installed_tools = {
+            "clang-format",
+            "prettier", -- prettier formatter
+            -- "stylua", -- lua formatter
+            "isort", -- python formatter
+            "black", -- python formatter
+            "mypy", -- python linter
+            "eslint_d", -- js linter
+            "markdownlint",
+            "jsonlint",
+        }
+
         local handle = io.popen("uname -a")
         if handle ~= nil then
             local u = require("utils")
             local os_name = handle:read("*a")
             handle:close()
 
-            local b = u.contains(os_name, "nixos")
-            if b then
-                table.insert(ensure_installed, "nil_ls")
+            local is_nixos = u.contains(os_name, "nixos")
+            if is_nixos then
+                -- 当系统为 nixos 时才安装 nil_ls
+                table.insert(ensure_installed_lsp_servers, "nil_ls")
+            else
+                -- 当系统非 nixos 时才安装 lua_ls / stylua
+                table.insert(ensure_installed_lsp_servers, "lua_ls")
+                table.insert(ensure_installed_tools, "stylua")
             end
-            -- print(vim.inspect(ensure_installed))
         end
 
         -- enable mason and configure icons
@@ -52,23 +67,13 @@ return {
 
         mason_lspconfig.setup({
             -- list of servers for mason to install
-            ensure_installed = ensure_installed,
+            ensure_installed = ensure_installed_lsp_servers,
             -- auto-install configured servers (with lspconfig)
             automatic_installation = true, -- not the same as ensure_installed
         })
 
         mason_tool_installer.setup({
-            ensure_installed = {
-                "clang-format",
-                "prettier", -- prettier formatter
-                "stylua", -- lua formatter
-                "isort", -- python formatter
-                "black", -- python formatter
-                "mypy", -- python linter
-                "eslint_d", -- js linter
-                "markdownlint",
-                "jsonlint",
-            },
+            ensure_installed = ensure_installed_tools,
         })
     end,
 }
