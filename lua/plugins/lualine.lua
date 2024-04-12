d4ad8ad6d5f986b31b2f85dfd8b0e899f55f8917
npc-z <1763998996@qq.com>
2024-04-12 21:15:13 +0800

    update lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,14 +7,14 @@ return {
     config = function()
         local lualine = require("lualine")
 
-        local count_cur_buf_words = require("wordcounter").count_cur_buf_words
-        local function words()
-            return "W:" .. count_cur_buf_words()
-        end
-
-        local function total_lines()
-            return "L:" .. vim.api.nvim_buf_line_count(0)
-        end
+        -- local count_cur_buf_words = require("wordcounter").count_cur_buf_words
+        -- local function words()
+        --     return "W:" .. count_cur_buf_words()
+        -- end
+        --
+        -- local function total_lines()
+        --     return "L:" .. vim.api.nvim_buf_line_count(0)
+        -- end
 
         lualine.setup({
             options = {
@@ -45,21 +45,21 @@ return {
                     },
                 },
                 lualine_x = {
-                    "filesize",
-                    total_lines,
-                    words,
+                    -- "filesize",
+                    -- total_lines,
+                    -- words,
                     {
                         "fileformat",
-                        -- symbols = {
-                        --   unix = '', -- e712
-                        --   dos = '', -- e70f
-                        --   mac = '', -- e711
-                        -- },
                         symbols = {
-                            unix = "LF",
-                            dos = "CRLF",
-                            mac = "CR",
+                            unix = "", -- e712
+                            dos = "", -- e70f
+                            mac = "", -- e711
                         },
+                        -- symbols = {
+                        --     unix = "LF",
+                        --     dos = "CRLF",
+                        --     mac = "CR",
+                        -- },
                     },
                     "encoding",
                     "filetype",
