803f71df83479ee88156ede1c4bfce570b523e01
npc-z <1763998996@qq.com>
2024-06-07 22:49:27 +0800

    [codeium] disalble codeium in come workspace

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,5 +1,11 @@
 return {
     "Exafunction/codeium.nvim",
+    enabled = function()
+        if vim.fn.getcwd():match("fuckdays") then
+            return false
+        end
+        return true
+    end,
     event = "VeryLazy",
     dependencies = {
         "nvim-lua/plenary.nvim",
