037165c48f5013e3c12505b0769dc23abe359b08
npc-z <1763998996@qq.com>
2024-06-16 10:31:56 +0800

    [codeium] only enable on thinkpd pc

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -4,9 +4,17 @@ return {
         if vim.fn.getcwd():match("fuckdays") then
             return false
         end
-        return true
+
+        local handle = io.popen("uname -a")
+        if handle ~= nil then
+            local u = require("utils")
+            local os_name = handle:read("*a")
+            handle:close()
+            return u.contains(os_name, "thinkpad")
+        end
     end,
-    event = "VeryLazy",
+    -- event = "VeryLazy",
+    event = "InsertEnter",
     dependencies = {
         "nvim-lua/plenary.nvim",
         "hrsh7th/nvim-cmp",
