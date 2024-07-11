55be12947b0410087e46109c0c3b3a792420cd39
npc-z <1763998996@qq.com>
2024-07-11 09:18:05 +0800

    [nvim-dbee] supoort sql editor

---
+++ lua/plugins/sql.lua
@@ -0,0 +1,45 @@
+return {
+    "kndndrj/nvim-dbee",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+    },
+    build = function()
+        -- Install tries to automatically detect the install method.
+        -- if it fails, try calling it with one of these parameters:
+        --    "curl", "wget", "bitsadmin", "go"
+        -- NOTE: install within neovim by nixos
+        -- require("dbee").install()
+    end,
+    config = function()
+        local dbee = require("dbee")
+
+        dbee.setup({
+            -- you can specify an optional default connection id and it will be the active one
+            -- when dbee starts
+            default_connection = nil,
+
+            drawer = {
+                -- show help or not
+                disable_help = true,
+            },
+
+            result = {
+                -- number of rows in the results set to display per page
+                page_size = 100,
+            },
+
+            -- window layout
+            window_layout = require("dbee.layouts").Default:new({
+                drawer_width = 30,
+                result_height = 12,
+                call_log_height = 12,
+            }),
+        })
+
+        local map = vim.api.nvim_set_keymap
+        local opts = { noremap = true, silent = true }
+
+        opts.desc = "[D]bee [T]oggle"
+        map("n", "<leader>dt", ":Dbee toggle<CR>", opts)
+    end,
+}
