45d9551430088b64adf51aa866966278eef59a41
npc-z <1763998996@qq.com>
2024-09-16 18:26:48 +0800

    [sql] setup keybinds

--- lua/plugins/sql.lua
+++ lua/plugins/sql.lua
@@ -41,5 +41,47 @@ return {
 
         opts.desc = "[D]bee [T]oggle"
         map("n", "<leader>dt", ":Dbee toggle<CR>", opts)
+
+        function select_paragraph_and_execute_bb()
+            vim.cmd("normal! vip")
+
+            -- 执行 BB 命令
+            vim.api.nvim_feedkeys(
+                vim.api.nvim_replace_termcodes("BB", true, true, true),
+                "v",
+                true
+            )
+            -- vim.cmd("wincmd k")
+            -- 创建一个定时器
+            local timer = vim.loop.new_timer()
+
+            -- 启动定时器，设置 500 毫秒后执行回调函数
+            timer:start(
+                500,
+                0,
+                vim.schedule_wrap(function()
+                    -- 执行 <C-w>k 的命令
+                    vim.cmd("wincmd k")
+
+                    -- 关闭定时器
+                    timer:stop()
+                    timer:close()
+                end)
+            )
+        end
+
+        -- 在 sql 文件类型中设置快捷键
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "sql",
+            callback = function()
+                vim.api.nvim_buf_set_keymap(
+                    0, -- 为当前 buffer 设置快捷键
+                    "n",
+                    "<C-CR>",
+                    ":lua select_paragraph_and_execute_bb()<CR>",
+                    { noremap = true, silent = true }
+                )
+            end,
+        })
     end,
 }
