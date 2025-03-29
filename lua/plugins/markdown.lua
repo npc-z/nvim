8c37980453a8a49a845be1f2b266d72c517db4f7
npc <1763998996@qq.com>
2025-03-29 10:38:02 +0800

    [ai] yetone/avante.nvim

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -3,7 +3,13 @@ return {
     "MeanderingProgrammer/markdown.nvim",
     name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
     dependencies = { "nvim-treesitter/nvim-treesitter" },
+    ft = { "markdown", "Avante" },
     config = function()
-        require("render-markdown").setup({})
+        require("render-markdown").setup({
+            file_types = {
+                "markdown",
+                "Avante", -- for AI plugin yetone/avante.nvim
+            },
+        })
     end,
 }
