ff01430e49d3a9ed2c6f03853d33b9fa74bdbded
npc-z <1763998996@qq.com>
2025-04-08 11:01:20 +0800

    fix auto-session config

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,13 +1,11 @@
 return {
     "rmagatti/auto-session",
+    lazy = false,
     config = function()
         local auto_session = require("auto-session")
 
         auto_session.setup({
-            auto_session_enabled = true,
-            auto_save_enabled = true,
-            auto_restore_enabled = true,
-            auto_session_suppress_dirs = {
+            suppressed_dirs = {
                 "~/",
                 "~/Projects",
                 "~/Downloads",
@@ -16,8 +14,6 @@ return {
                 "~/github",
                 "~/.local/share",
             },
-            -- the configs below are lua only
-            bypass_session_save_file_types = {},
         })
         vim.o.sessionoptions =
             "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
