6f769591a60143a7e83ae65a5dd290cbe7fd6784
npc-z <1763998996@qq.com>
2025-08-04 14:04:06 +0800

    vim.diagnostic.go_prev/go_next => jump

--- lua/plugins/lsp/after/lspkeymaps.lua
+++ lua/plugins/lsp/after/lspkeymaps.lua
@@ -71,11 +71,17 @@ M.setup_keymaps = function(bufnr)
     opts.desc = "Show line diagnostics"
     keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
+    -- jump to previous diagnostic in buffer
     opts.desc = "Go to previous diagnostic"
-    keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+    keymap.set("n", "[d", function()
+        vim.diagnostic.jump({ count = -1, float = true })
+    end, opts)
 
+    -- jump to next diagnostic in buffer
     opts.desc = "Go to next diagnostic"
-    keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+    keymap.set("n", "]d", function()
+        vim.diagnostic.jump({ count = 1, float = true })
+    end, opts)
 
     opts.desc = "Show documentation for what is under cursor"
     keymap.set("n", "gh", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
