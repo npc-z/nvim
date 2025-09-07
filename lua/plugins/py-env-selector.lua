589d448b5daacc2f7907522e764b01378ebc1859
npc <1763998996@qq.com>
2025-09-07 09:32:30 +0800

    fix py-env-selector config

--- lua/plugins/py-env-selector.lua
+++ lua/plugins/py-env-selector.lua
@@ -12,7 +12,6 @@ return {
         },
     },
     lazy = false,
-    branch = "regexp", -- This is the regexp branch, use this for the new version
     keys = {
         -- open VenvSelector to pick a venv.
         { "<leader>vs", "<cmd>VenvSelect<cr>" },
