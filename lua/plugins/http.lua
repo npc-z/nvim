af4d980f86c2217a0fa82ede25ddffab3c357f7d
npc-z <1763998996@qq.com>
2025-09-22 13:18:37 +0800

    [http] replace htt-client.nvim with kulala.nvim

--- lua/plugins/http.lua
+++ lua/plugins/http.lua
@@ -1,20 +1,20 @@
 return {
-    "askfiy/http-client.nvim",
+    "mistweaverco/kulala.nvim",
+    ft = { "http", "rest" },
+
     config = function()
-        require("http-client").setup()
+        local kulala = require("kulala")
+        kulala.setup()
 
         -- 在 http 文件类型中设置快捷键
         vim.api.nvim_create_autocmd("FileType", {
             pattern = "http",
             callback = function()
                 -- 为当前 buffer 设置快捷键
-                vim.api.nvim_buf_set_keymap(
-                    0,
-                    "n",
-                    "<C-CR>",
-                    ":HttpClient sendRequest<CR>",
-                    { noremap = true, silent = true }
-                )
+                vim.keymap.set("n", "<C-CR>", function()
+                    -- vim.keymap.set("n", "<leader>rs", function()
+                    kulala.run()
+                end, { desc = "Send request" })
             end,
         })
     end,
