a1c22a0285ad61eb54d4a568c6c7c91f1261f8e3
npc <1763998996@qq.com>
2025-09-27 22:44:46 +0800

    autopairs: Expand multiple pairs on enter key

--- lua/plugins/nvim-aotupairs.lua
+++ lua/plugins/nvim-aotupairs.lua
@@ -6,6 +6,51 @@ return {
     },
     config = function()
         local autopairs = require("nvim-autopairs")
+        local Rule = require("nvim-autopairs.rule")
+        local npairs = require("nvim-autopairs")
+        local cond = require("nvim-autopairs.conds")
+
+        -- https://github.com/rstacruz/vim-closer/blob/master/autoload/closer.vim
+        -- https://github.com/windwp/nvim-autopairs/wiki/Custom-rules#expand-multiple-pairs-on-enter-key
+        -- Expand multiple pairs on enter key
+        local get_closing_for_line = function(line)
+            local i = -1
+            local clo = ""
+
+            while true do
+                i, _ = string.find(line, "[%(%)%{%}%[%]]", i + 1)
+                if i == nil then
+                    break
+                end
+                local ch = string.sub(line, i, i)
+                local st = string.sub(clo, 1, 1)
+
+                if ch == "{" then
+                    clo = "}" .. clo
+                elseif ch == "}" then
+                    if st ~= "}" then
+                        return ""
+                    end
+                    clo = string.sub(clo, 2)
+                elseif ch == "(" then
+                    clo = ")" .. clo
+                elseif ch == ")" then
+                    if st ~= ")" then
+                        return ""
+                    end
+                    clo = string.sub(clo, 2)
+                elseif ch == "[" then
+                    clo = "]" .. clo
+                elseif ch == "]" then
+                    if st ~= "]" then
+                        return ""
+                    end
+                    clo = string.sub(clo, 2)
+                end
+            end
+
+            return clo
+        end
 
         autopairs.setup({
             check_ts = true, -- enable treesitter
@@ -15,6 +60,20 @@ return {
                 java = false, -- don't check treesitter on java
             },
         })
+
+        autopairs.remove_rule("(")
+        autopairs.remove_rule("{")
+        autopairs.remove_rule("[")
+
+        autopairs.add_rule(Rule("[%(%{%[]", "")
+            :use_regex(true)
+            :replace_endpair(function(opts)
+                return get_closing_for_line(opts.line)
+            end)
+            :end_wise(function(opts)
+                -- Do not endwise if there is no closing
+                return get_closing_for_line(opts.line) ~= ""
+            end))
 
         -- import nvim-autopairs completion functionality
         local cmp_autopairs = require("nvim-autopairs.completion.cmp")
