e0a759582a326b1f81023c1fe247a57477dea871
npc-z <1763998996@qq.com>
2026-04-04 22:19:03 +0800

    feat(tree-sitter): 升级以适配 nvim 0.12

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -8,52 +8,35 @@ return {
         "windwp/nvim-ts-autotag",
     },
 
-    config = function()
-        local treesitter = require("nvim-treesitter.configs")
-
-        treesitter.setup({
-            -- enable syntax highlighting
-            highlight = {
-                enable = true,
-            },
-            -- enable indentation
-            indent = { enable = true },
-            -- enable autotagging (w/ nvim-ts-autotag plugin)
-            autotag = {
-                enable = true,
-            },
-            -- ensure these language parsers are installed
-            ensure_installed = {
-                "python",
-                "go",
-                "http",
-                "json",
-                "javascript",
-                "typescript",
-                "tsx",
-                "yaml",
-                "html",
-                "css",
-                "markdown",
-                "markdown_inline",
-                "graphql",
-                "bash",
-                "lua",
-                "vim",
-                "dockerfile",
-                "gitignore",
-                "query",
-                "just",
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = false, -- set to `false` to disable one of the mappings
-                    scope_incremental = "grc",
-                    node_incremental = "<Enter>",
-                    node_decremental = "<bs>",
-                },
-            },
-        })
+    init = function()
+        local ensureInstalled = {
+            "python",
+            "go",
+            "http",
+            "json",
+            "javascript",
+            "typescript",
+            "tsx",
+            "yaml",
+            "html",
+            "css",
+            "markdown",
+            "markdown_inline",
+            "graphql",
+            "bash",
+            "lua",
+            "vim",
+            "dockerfile",
+            "gitignore",
+            "query",
+            "just",
+        }
+        local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+        local parsersToInstall = vim.iter(ensureInstalled)
+            :filter(function(parser)
+                return not vim.tbl_contains(alreadyInstalled, parser)
+            end)
+            :totable()
+        require("nvim-treesitter").install(parsersToInstall)
     end,
 }
