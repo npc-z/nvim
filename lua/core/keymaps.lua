3dfc4953160b99f8784310bfe41fc0a9dc3b6ea5
npc-z <1763998996@qq.com>
2026-04-07 21:54:07 +0800

    fix(undotree): load and set keymap and remove unset undodir

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -157,19 +157,11 @@ map("n", "<leader>q", ":Neotree close<CR>:q<CR>", opt)
 map("n", "<leader>uw", "gUiw", opt)
 map("n", "<leader>lw", "guiw", opt)
 
--- 保存 undotree 到本地文件
-vim.cmd([[
-    if has("persistent_undo")
-       let target_path = expand("~/.local/share/nvim/undodir")
-            if !isdirectory(target_path)
-                call mkdir(target_path, "p", 0700)
-            endif
-
-        let &undodir=target_path
-        set undofile
-    endif
-]])
-vim.keymap.set("n", "<leader>u", function()
-    vim.cmd.packadd("nvim.undotree")
-    vim.cmd.Undotree()
-end, { desc = "Toggle Undotree" })
+-- load undotree built-in plugin and set keymap
+vim.cmd("packadd nvim.undotree")
+vim.keymap.set(
+    "n",
+    "<leader>ut",
+    require("undotree").open,
+    { desc = "Toggle Undotree" }
+)
