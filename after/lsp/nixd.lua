32660cdf392bfa5b0226c3b0620c1647e07e0a21
npc-z <1763998996@qq.com>
2026-04-10 16:59:30 +0800

    fix(lsp): update nixd setting's options

--- after/lsp/nixd.lua
+++ after/lsp/nixd.lua
@@ -1,3 +1,12 @@
+local hostname = vim.trim(vim.fn.system("hostname"))
+local home = vim.env.HOME
+local flake = string.format("(builtins.getFlake \"%s/.config/nixos\")", home)
+
+local is_darwin = vim.fn.has("macunix") == 1
+
+local config_key = is_darwin and string.format("darwinConfigurations.%s", hostname)
+    or string.format("nixosConfigurations.%s", hostname)
+
 ---@type vim.lsp.Config
 return {
     cmd = { "nixd" },
@@ -11,10 +20,14 @@ return {
             },
             options = {
                 nixos = {
-                    expr = '(builtins.getFlake "/home/npc/.config/nixos").nixosConfigurations.ser7-nixos.options',
+                    expr = string.format("%s.%s.options", flake, config_key),
                 },
-                home_manager = {
-                    expr = '(builtins.getFlake "/home/npc/.config/nixos").homeConfigurations.ser7-nixos.options',
+                ["home-manager"] = {
+                    expr = string.format(
+                        "%s.%s.options.home-manager.users.type.getSubOptions []",
+                        flake,
+                        config_key
+                    ),
                 },
             },
         },
