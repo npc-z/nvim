5eb88a67bda5431bcfba4159d3db286188ca3cb8
npc-z <1763998996@qq.com>
2026-04-12 09:43:29 +0800

    feat(lsp): update haskell/rust plugin version

--- lua/plugins/lsp/rust.lua
+++ lua/plugins/lsp/rust.lua
@@ -1,6 +1,6 @@
 return {
     "mrcjkb/rustaceanvim",
-    version = "^6",
+    version = "^9",
     lazy = false,
     dependencies = {
         {
@@ -9,17 +9,6 @@ return {
         },
     },
     config = function()
-        vim.g.rustaceanvim = {
-            server = {
-                -- on_attach is handled by the global LspAttach autocmd
-                -- in lua/plugins/lsp/lspconfig.lua
-                default_settings = {
-                    -- rust-analyzer language server configuration
-                    -- ["rust-analyzer"] = {},
-                },
-            },
-        }
-
         require("crates").setup({
             lsp = {
                 enabled = true,
