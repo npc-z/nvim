e72da1d192619d5eaa75342ad512d71db4330ef0
npc-z <1763998996@qq.com>
2026-05-09 17:28:29 +0800

    feat(lua_ls): format file and add `Snacks` to globals

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -5,32 +5,36 @@ return {
             local path = client.workspace_folders[1].name
             if
                 path ~= vim.fn.stdpath("config")
-                and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+                and (
+                    vim.uv.fs_stat(path .. "/.luarc.json")
+                    or vim.uv.fs_stat(path .. "/.luarc.jsonc")
+                )
             then
                 return
             end
         end
 
-        client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-            runtime = {
-                version = "LuaJIT",
-                path = {
-                    "lua/?.lua",
-                    "lua/?/init.lua",
+        client.config.settings.Lua =
+            vim.tbl_deep_extend("force", client.config.settings.Lua, {
+                runtime = {
+                    version = "LuaJIT",
+                    path = {
+                        "lua/?.lua",
+                        "lua/?/init.lua",
+                    },
                 },
-            },
-            workspace = {
-                checkThirdParty = false,
-                library = {
-                    vim.env.VIMRUNTIME,
+                workspace = {
+                    checkThirdParty = false,
+                    library = {
+                        vim.env.VIMRUNTIME,
+                    },
                 },
-            },
-        })
+            })
     end,
     settings = {
         Lua = {
             diagnostics = {
-                globals = { "vim" },
+                globals = { "Snacks", "vim" },
             },
         },
     },
